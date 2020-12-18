f155fd6020c5205999e003f4a0196608216af8f1
Alvin <alvincheong@u.nus.edu>
2020-12-18 14:09:27 +0800

    using multipart only

--- frontend/src/app/components/main.component.ts
+++ frontend/src/app/components/main.component.ts
@@ -19,7 +19,8 @@ export class MainComponent implements OnInit {
 	constructor(private cameraSvc: CameraService, private fb: FormBuilder, private auth: Auth, private http: HttpClient) { }
 
 	ngOnInit(): void {
-	  if (this.cameraSvc.hasImage()) {
+
+		if (this.cameraSvc.hasImage()) {
 		  const img = this.cameraSvc.getImage()
 		  this.img = img
 		  this.imagePath = img.imageAsDataUrl
@@ -35,6 +36,8 @@ export class MainComponent implements OnInit {
 	}
 
 	clear() {
+		this.img = null
+		this.cameraSvc.clear()
 		this.imagePath = '/assets/cactus.png'
 		this.mainForm.reset()
 	}
@@ -43,48 +46,24 @@ export class MainComponent implements OnInit {
 		if (await this.auth.verifyLogin(this.auth.username, this.auth.password)){
 
 			// post image to S3
-			var digitalOceanKey = {}
-			var imageUpload = false
+			// var imageUpload = false
 			const formData = new FormData();
-			formData.set('image-file', this.img.imageData);
-			await this.http.post<any>('/uploadImage', formData).toPromise().then(
+			formData.set('image-file', this.img.imageData)
+			
+			const params = new HttpParams()
+			.set('title', this.mainForm.get('title').value)
+			.set('comments', this.mainForm.get('comments').value)
+
+			await this.http.post<any>('/share', formData, {params: params}).toPromise().then(
 				function(response) {
 				  // success callback
-					digitalOceanKey = response
-					imageUpload = true;
-				},
+					window.alert(`Post uploaded. ID:  ${response['ops'][0]._id}`)					
+			},
 				function(response) {
-				  // failure callback,handle error here
-				  // response.data.message will be "This is an error!"
-				  imageUpload = false
 				  window.alert(response.error.message)
 				}
 			)  
-
-			if (imageUpload){
-				const body = new HttpParams()
-				.set('title', this.mainForm.get('title').value)
-				.set('comments', this.mainForm.get('comments').value)
-				.set('digitalOceanKey', digitalOceanKey['key'])
-			
-				const httpHeaders = new HttpHeaders()
-				.set('Content-Type', 'application/x-www-form-urlencoded')
-			
-				await this.http.post('/share', body, {headers: httpHeaders}).toPromise().then(
-					function(response) {
-					  // success callback
-					//   window.alert(response.error.message)
-					console.info(response)
-					window.alert(`Post uploaded. ID:  ${response['ops'][0]._id}`)					
-					},
-					function(response) {
-					  // failure callback,handle error here
-						window.alert(response.error.message)
-					}
-				  )	
-				this.clear()
-			}
-
+			this.clear()
 		}
 		else {
 			window.alert('Credentials are not correct')
